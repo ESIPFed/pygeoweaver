c20e1dfdc5d301d839bddf138652284e6ef9ff24
Gokul Prathin <gokulprathin8@gmail.com>
2023-04-15 04:32:29 -0400

    refactor

--- pygeoweaver/__main__.py
+++ pygeoweaver/__main__.py
@@ -2,25 +2,29 @@
 The main function of pygeoweaver
 To run in CLI mode. 
 """
-from pygeoweaver.history_process import show_process_history
-from pygeoweaver.list_hosts import list_hosts
-from pygeoweaver.list_processes import list_processes
-from pygeoweaver.list_workflows import list_workflows
-from pygeoweaver.server import start, stop
+
+# replaced with __init__.py
 
 
-def main():
-    # start geoweaver
-    # start()
-    # stop geoweaver
-    # stop()
-    # list resources
-    # list_hosts()
-    # list_processes()
-    # list_workflows()
-    # show history
-    show_process_history("gfvnp8a7rgh")
-
-
-if __name__ == "__main__":
-    main()
+# from pygeoweaver.history_process import show_process_history
+# from pygeoweaver.list_hosts import list_hosts
+# from pygeoweaver.list_processes import list_processes
+# from pygeoweaver.list_workflows import list_workflows
+# from pygeoweaver.server import start, stop
+#
+#
+# def main():
+#     # start geoweaver
+#     # start()
+#     # stop geoweaver
+#     # stop()
+#     # list resources
+#     # list_hosts()
+#     # list_processes()
+#     # list_workflows()
+#     # show history
+#     show_process_history("gfvnp8a7rgh")
+#
+#
+# if __name__ == "__main__":
+#     main()
