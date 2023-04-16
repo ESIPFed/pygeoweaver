416990381453398a1d25d76d98869a54130c0fca
Ziheng Sun <zsun@gmu.edu>
2023-04-16 17:19:54 -0400

    test tags

--- pygeoweaver/__main__.py
+++ pygeoweaver/__main__.py
@@ -11,13 +11,13 @@ from pygeoweaver import start, stop
 
 def main():
     # start geoweaver
-    #start()
+    start()
     # stop geoweaver
     # stop()
     # list resources
     #list_hosts()
     #list_processes()
-    list_workflows()
+    # list_workflows()
     # show history
     #show_history("ll3u3W78eOEfklxhBJ")
     # detail host
@@ -28,9 +28,9 @@ def main():
     # import workflow
     #import_workflow("/Users/joe/Downloads/gr3ykr8dynu12vrwq11oy.zip")
     # export workflow
-    export_workflow("gr3ykr8dynu12vrwq11oy", "4", "/Users/joe/Downloads/test_pygeoweaver_export.zip")
+    # export_workflow("gr3ykr8dynu12vrwq11oy", "4", "/Users/joe/Downloads/test_pygeoweaver_export.zip")
     # run workflow
-    
+
 
 if __name__ == "__main__":
      main()
