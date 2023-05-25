09cb8b89bd60756b824352d95f4289a2c4fc730d
Ziheng Sun <zsun@gmu.edu>
2023-05-25 14:28:46 -0400

    Update test_detail.py

--- test/test_detail.py
+++ test/test_detail.py
@@ -13,23 +13,20 @@ logger = get_logger(__name__)
 def test_detail_process(capfd):
     detail_process("not_existing_id")
     output, err = capfd.readouterr()
-    logger.info("stdout_output"+output)
-    logger.info("stderr_output"+err)
+    logger.debug("stdout_output"+output)
     assert "No process found with id: not_existing_id" in output
 
 
 def test_detail_workflow(capfd):
     detail_workflow("not_existing_id")
     output, err = capfd.readouterr()
-    logger.info("stdout_output"+output)
-    logger.info("stderr_output"+err)
+    logger.debug("stdout_output"+output)
     assert "No workflow found with id: not_existing_id" in output
 
 
 def test_detail_host(capfd):
     detail_host("not_existing_id")
     output, err = capfd.readouterr()
-    logger.info("stdout_output"+output)
-    logger.info("stderr_output"+err)
+    logger.debug("stdout_output"+output)
     assert "No host found with id: not_existing_id" in output
 
