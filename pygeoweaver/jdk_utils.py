fd7a86da80167030f244f73dd1b89bcb031dfcc6
Ziheng Sun <zsun@gmu.edu>
2023-06-04 18:41:08 -0400

    Update jdk_utils.py

--- pygeoweaver/jdk_utils.py
+++ pygeoweaver/jdk_utils.py
@@ -9,116 +9,115 @@ import urllib.request
 
 
 def install_jdk():
-    # Check the platform
     system = platform.system()
+    architecture = platform.machine()
 
-    if system == 'Darwin':  # macOS
-        install_jdk_macos()
+    if system == 'Darwin':
+        if architecture == 'x86_64':
+            install_jdk_macos('11.0.18-10', 'jdk_x64_mac_hotspot')
+        elif architecture == 'arm64':
+            install_jdk_macos('11.0.18-10', 'jdk_aarch64_mac_hotspot')
+        else:
+            print('Unsupported architecture.')
 
-    elif system == 'Linux':  # Linux
-        install_jdk_linux()
+    elif system == 'Linux':
+        # https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.18%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.18_10.tar.gz
+        if architecture == 'x86_64':
+            install_jdk_linux('11.0.18-10', 'jdk_x64_linux_hotspot')
+        elif architecture == 'aarch64':
+            install_jdk_linux('11.0.18-10', 'jdk_aarch64_linux_hotspot')
+        else:
+            print('Unsupported architecture.')
 
-    elif system == 'Windows':  # Windows
-        install_jdk_windows()
+    elif system == 'Windows':
+        if architecture == 'AMD64' or architecture == 'x86_64':
+            install_jdk_windows('11.0.18-10', 'jdk_x64_windows_hotspot')
+        elif architecture == 'x86-32':
+            install_jdk_windows('11.0.18-10', 'jdk_x86-32_windows_hotspot')
+        else:
+            print('Unsupported architecture.')
 
     else:
         print('Unsupported platform.')
 
 
-def install_jdk_macos():
-    jdk_version = 'adopt@1.11.0-8'
-    jdk_url = f'https://github.com/adoptium/temurin11-binaries/releases/download/{jdk_version}/OpenJDK11U-jdk_x64_mac_hotspot_{jdk_version.replace("@", "-")}.tar.gz'
+def install_jdk_macos(jdk_version, jdk_arch):
+    # jdk_aarch64_linux_hotspot
+    jdk_url = f'https://github.com/adoptium/temurin11-binaries/releases/download/{jdk_version}/OpenJDK11U-{jdk_arch}_{jdk_version.replace("-", "_")}.tar.gz'
+    jdk_install_dir = os.path.expanduser('~/jdk')
 
-    jdk_file_name = jdk_url.split('/')[-1]
-    jdk_dir_name = jdk_file_name.replace('.tar.gz', '')
+    # Download JDK archive
+    download_file(jdk_url, 'jdk.tar.gz')
 
-    # Download JDK
-    print('Downloading JDK...')
-    urllib.request.urlretrieve(jdk_url, jdk_file_name)
+    # Extract JDK archive
+    extract_tar_archive('jdk.tar.gz', jdk_install_dir)
 
-    # Extract JDK
-    print('Extracting JDK...')
-    with tarfile.open(jdk_file_name, 'r:gz') as tar:
-        tar.extractall()
+    # Set JDK environment variables
+    set_jdk_env_vars(jdk_install_dir)
 
-    # Move JDK to user's home directory
-    home_dir = os.path.expanduser('~')
-    jdk_install_dir = os.path.join(home_dir, jdk_dir_name)
-    shutil.move(jdk_dir_name, jdk_install_dir)
 
-    # Set environment variables
+def install_jdk_linux(jdk_version, jdk_arch):
+    jdk_url = f'https://github.com/adoptium/temurin11-binaries/releases/download/{jdk_version}/OpenJDK11U-{jdk_arch}_{jdk_version.replace("-", "_")}.tar.gz'
+    jdk_install_dir = os.path.expanduser('~/jdk')
+
+    # Download JDK archive
+    download_file(jdk_url, 'jdk.tar.gz')
+
+    # Extract JDK archive
+    extract_tar_archive('jdk.tar.gz', jdk_install_dir)
+
+    # Set JDK environment variables
+    set_jdk_env_vars(jdk_install_dir)
+
+
+def install_jdk_windows(jdk_version, jdk_arch):
+    jdk_url = f'https://github.com/adoptium/temurin11-binaries/releases/download/{jdk_version}/OpenJDK11U-{jdk_arch}_{jdk_version.replace("-", "_")}.zip'
+    jdk_install_dir = os.path.expanduser('~/jdk')
+
+    # Download JDK archive
+    download_file(jdk_url, 'jdk.zip')
+
+    # Extract JDK archive
+    extract_zip_archive('jdk.zip', jdk_install_dir)
+
+    # Set JDK environment variables
+    set_jdk_env_vars(jdk_install_dir)
+
+
+def download_file(url, filename):
+    print(f'Downloading {filename}...')
+    urllib.request.urlretrieve(url, filename)
+    print(f'{filename} downloaded.')
+
+
+def extract_tar_archive(archive_path, destination_dir):
+    print(f'Extracting {archive_path}...')
+    with tarfile.open(archive_path, 'r:gz') as tar_ref:
+        tar_ref.extractall(destination_dir)
+    print(f'{archive_path} extracted to {destination_dir}.')
+
+
+def extract_zip_archive(archive_path, destination_dir):
+    print(f'Extracting {archive_path}...')
+    with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+        zip_ref.extractall(destination_dir)
+    print(f'{archive_path} extracted to {destination_dir}.')
+
+
+def set_jdk_env_vars(jdk_install_dir):
+    print(f'Setting JDK environment variables...')
+
+    # Update PATH environment variable
+    path_var = os.environ.get('PATH', '')
+    jdk_bin_path = os.path.join(jdk_install_dir, 'bin')
+    path_var = f'{jdk_bin_path};{path_var}' if platform.system() == 'Windows' else f'{jdk_bin_path}:{path_var}'
+    os.environ['PATH'] = path_var
+
+    # Update JAVA_HOME environment variable
     os.environ['JAVA_HOME'] = jdk_install_dir
-    os.environ['PATH'] = f'{jdk_install_dir}/bin:{os.environ["PATH"]}'
 
-    print('JDK installation completed.')
+    print('JDK environment variables set.')
 
-
-def install_jdk_linux():
-    # https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.19%2B7/OpenJDK11U-debugimage_aarch64_linux_hotspot_11.0.19_7.tar.gz
-    jdk_version = '11.0.19-7'
-    jdk_url = f'https://github.com/adoptium/temurin11-binaries/releases/download/jdk-{jdk_version}/OpenJDK11U-jdk_x64_linux_hotspot_{jdk_version.replace("-", "_")}.tar.gz'
-
-    jdk_file_name = jdk_url.split('/')[-1]
-    jdk_dir_name = jdk_file_name.replace('.tar.gz', '')
-
-    # Download JDK
-    print('Downloading JDK...')
-    print('jdk_url: ', jdk_url)
-    urllib.request.urlretrieve(jdk_url, jdk_file_name)
-
-    # Extract JDK
-    print('Extracting JDK...')
-    with tarfile.open(jdk_file_name, 'r:gz') as tar:
-        tar.extractall()
-
-    # Move JDK to user's home directory
-    home_dir = os.path.expanduser('~')
-    jdk_install_dir = os.path.join(home_dir, jdk_dir_name)
-    shutil.move(jdk_dir_name, jdk_install_dir)
-
-    # Set environment variables
-    with open(os.path.join(home_dir, '.bashrc'), 'a') as bashrc:
-        bashrc.write(f'\nexport JAVA_HOME="{jdk_install_dir}"\nexport PATH="$JAVA_HOME/bin:$PATH"\n')
-
-    print('JDK installation completed. Please restart your terminal or run "source ~/.bashrc" to apply the changes.')
-
-
-def install_jdk_windows():
-    jdk_version = 'adopt@1.11.0-8'
-    jdk_url = f'https://github.com/adoptium/temurin11-binaries/releases/download/{jdk_version}/OpenJDK11U-jdk_x64_windows_hotspot_{jdk_version.replace("@", "-")}.zip'
-
-    jdk_file_name = jdk_url.split('/')[-1]
-    jdk_dir_name = jdk_file_name.replace('.zip', '')
-
-    # Download JDK
-    print('Downloading JDK...')
-    urllib.request.urlretrieve(jdk_url, jdk_file_name)
-
-    # Extract JDK
-    print('Extracting JDK...')
-    with zipfile.ZipFile(jdk_file_name, 'r') as zip_ref:
-        zip_ref.extractall()
-
-    # Move JDK to user's home directory
-    home_dir = os.path.expanduser('~')
-    jdk_install_dir = os.path.join(home_dir, jdk_dir_name)
-    shutil.move(jdk_dir_name, jdk_install_dir)
-
-    # Set environment variables
-    subprocess.run(['setx', 'JAVA_HOME', jdk_install_dir])
-    os.environ['JAVA_HOME'] = jdk_install_dir
-    os.environ['PATH'] = f'{jdk_install_dir}\\bin;{os.environ["PATH"]}'
-
-    print('JDK installation completed. Please restart your command prompt to apply the changes.')
-
-
-def is_java_installed():
-    try:
-        # Check if Java is installed by running "java -version" command
-        subprocess.run(["java", "-version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return True
-    except:
-        return False
 
 
 def install_java():
@@ -156,9 +155,23 @@ def install_java():
         sys.exit(1)
 
 
+
+def is_java_installed():
+    try:
+        # Check if Java is installed by running "java -version" command
+        subprocess.run(["java", "-version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except:
+        return False
+    
+
+
 def check_java():
     # Check if Java is installed
     if not is_java_installed():
         print("Java is not installed. Installing...")
         install_jdk()
         print("Java installation complete.")
+
+
+
