41822e674c1bc7f0bb653c8d08436ba1a419542d
Gokul Prathin <gokulprathin8@gmail.com>
2023-06-13 17:43:37 -0400

    optimize run workflow

--- pygeoweaver/sc_run.py
+++ pygeoweaver/sc_run.py
@@ -65,6 +65,7 @@ def run_process(
 
 
 def run_workflow(
+        *,
         workflow_id: str,
         workflow_folder_path: str = None,
         workflow_zip_file_path: str = None,
@@ -73,6 +74,20 @@ def run_workflow(
         password_list: str = None,
         sync_path: os.PathLike = None,
 ):
+    """
+    Usage: <main class> run workflow [-d=<workflowFolderPath>]
+                                    [-f=<workflowZipPath>] [-e=<envs>]...
+                                    [-h=<hostStrings>]... [-p=<passes>]...
+                                    <workflowId>
+        <workflowId>           workflow id to run
+    -d, --workflow-folder-path=<workflowFolderPath>
+                                geoweaver workflow folder path
+    -e, --environments=<envs>  environments to run on. List of environment ids with comma as separator
+    -f, --workflow-zip-file-path=<workflowZipPath>
+                                workflow package or path to workflow zip to run
+    -h, --hosts=<hostStrings>  hosts to run on. list of host ids with comma as separator.
+    -p, --passwords=<passes>   passwords to the target hosts. list of passwords with comma as separator.
+    """
     download_geoweaver_jar()
 
     if password_list is None:
@@ -80,18 +95,27 @@ def run_workflow(
 
     if sync_path:
         from . import sync_workflow
+
         sync_workflow(workflow_id=workflow_id, sync_to_path=sync_path)
 
     if not workflow_id and not workflow_folder_path and not workflow_zip_file_path:
-        raise RuntimeError("Please provide at least one of the three options: workflow id, folder path, or zip path")
+        raise RuntimeError(
+            "Please provide at least one of the three options: workflow id, "
+            "folder path or zip path"
+        )
 
-    command = [get_java_bin_path(), "-jar", get_geoweaver_jar_path(), "run", "workflow", workflow_id]
-    if workflow_folder_path:
-        command.extend(["-d", workflow_folder_path])
-    if workflow_zip_file_path:
-        command.extend(["-f", workflow_zip_file_path])
-    if host_list:
-        command.extend(["-h"] + host_list.split(","))
-    if password_list:
-        command.extend(["-p"] + password_list.split(","))
-    subprocess.run(command, cwd=get_root_dir())
+    if workflow_id and not workflow_folder_path and not workflow_zip_file_path:
+        command = [get_java_bin_path(), "-jar", get_geoweaver_jar_path(), "run", "workflow",
+                   workflow_id, "-h", host_list, "-p", password_list]
+        subprocess.run(command, cwd=f"{get_root_dir()}/")
+
+    if workflow_folder_path and not workflow_zip_file_path:
+        # command to run workflow from folder
+        command = [get_java_bin_path(), "-jar", get_geoweaver_jar_path(), "run", "workflow", workflow_id,
+                   "-d", workflow_folder_path, "-h", host_list, "-p", password_list]
+        subprocess.run(command, cwd=f"{get_root_dir()}/")
+
+    if not workflow_folder_path and workflow_zip_file_path:
+        command = [get_java_bin_path(), "-jar", get_geoweaver_jar_path(), "run", "workflow", workflow_id, "-f",
+                   workflow_zip_file_path, "-h", host_list, "-p", password_list]
+        subprocess.run(command, cwd=f"{get_root_dir()}/")
