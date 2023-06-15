33d724ba5bc99c0b1936b69f798650cc246febf3
Ziheng Sun <zsun@gmu.edu>
2023-06-14 21:33:40 -0400

    use reset password

--- pygeoweaver/sc_resetpassword.py
+++ pygeoweaver/sc_resetpassword.py
@@ -1,10 +1,21 @@
 import subprocess
 from pygeoweaver.utils import (
+    check_ipython,
     download_geoweaver_jar,
     get_geoweaver_jar_path,
     get_java_bin_path,
     get_root_dir,
 )
+
+def get_password_twice():
+    while True:
+        password1 = getpass('Enter password: ')
+        password2 = getpass('Re-enter password: ')
+        
+        if password1 == password2:
+            return password1
+        else:
+            print("Passwords don't match. Please try again.")
 
 
 def reset_password():
@@ -13,11 +24,25 @@ def reset_password():
     Reset password for localhost
     """
     download_geoweaver_jar()
-    subprocess.run(
-        [
-            get_java_bin_path(),
-            "-jar",
-            get_geoweaver_jar_path(),
-            "resetpassword",
-        ]
-    )
+    
+    if check_ipython():
+        password = get_password_twice()
+        subprocess.run(
+            [
+                get_java_bin_path(),
+                "-jar",
+                get_geoweaver_jar_path(),
+                "resetpassword",
+                "-p",
+                password
+            ]
+        )
+    else:
+        subprocess.run(
+            [
+                get_java_bin_path(),
+                "-jar",
+                get_geoweaver_jar_path(),
+                "resetpassword",
+            ]
+        )
