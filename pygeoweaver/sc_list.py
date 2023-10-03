ad2a4c6e808da0f3ddc887e75e89091f8b94ea6a
Gokul Prathin <gokulprathin8@gmail.com>
2023-10-03 04:16:14 -0400

    changes to list commands to use spring apis

--- pygeoweaver/sc_list.py
+++ pygeoweaver/sc_list.py
@@ -2,32 +2,55 @@ import json
 
 import requests
 import subprocess
+
+from IPython.core.display_functions import display
+from ipywidgets import HTML
+
 from pygeoweaver.constants import *
 from pygeoweaver.utils import (
     download_geoweaver_jar,
     get_geoweaver_jar_path,
     get_java_bin_path,
     get_root_dir,
-    check_ipython,
+    check_ipython, create_table,
 )
 import pandas as pd
 
 
 def list_hosts():
     download_geoweaver_jar()
-    subprocess.run(
-        [get_java_bin_path(), "-jar", get_geoweaver_jar_path(), "list", "--host"],
-        cwd=f"{get_root_dir()}/",
-    )
+    url = f"{GEOWEAVER_DEFAULT_ENDPOINT_URL}/web/list"
+    form_data = {'type': 'host'}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = requests.post(url=url, headers=headers, data=form_data)
+
+    if 'get_ipython' in globals():
+        # Running in a Jupyter Notebook, display as HTML table
+        data_json = data.json()
+        table_html, _ = create_table(data_json)
+        display(HTML(table_html))
+    else:
+        # Not running in a Jupyter Notebook, display as Pandas DataFrame
+        data_json = data.json()
+        df = pd.DataFrame(data_json)
+        return df
 
 
 def list_processes():
     download_geoweaver_jar()
-    subprocess.run(["chmod", "+x", get_geoweaver_jar_path()], cwd=f"{get_root_dir()}/")
-    subprocess.run(
-        [get_java_bin_path(), "-jar", get_geoweaver_jar_path(), "list", "--process"],
-        cwd=f"{get_root_dir()}/",
-    )
+    url = f"{GEOWEAVER_DEFAULT_ENDPOINT_URL}/web/list"
+    form_data = {'type': 'process'}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = requests.post(url=url, headers=headers, data=form_data)
+
+    if 'get_ipython' in globals():
+        data_json = data.json()
+        table_html, _ = create_table(data_json)
+        display(HTML(table_html))
+    else:
+        data_json = data.json()
+        df = pd.DataFrame(data_json)
+        return df
 
 
 def list_processes_in_workflow(workflow_id):
@@ -48,7 +71,16 @@ def list_processes_in_workflow(workflow_id):
 
 def list_workflows():
     download_geoweaver_jar()
-    subprocess.run(
-        [get_java_bin_path(), "-jar", get_geoweaver_jar_path(), "list", "--workflow"],
-        cwd=f"{get_root_dir()}/",
-    )
+    url = f"{GEOWEAVER_DEFAULT_ENDPOINT_URL}/web/list"
+    form_data = {'type': 'workflow'}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = requests.post(url=url, headers=headers, data=form_data)
+
+    if 'get_ipython' in globals():
+        data_json = data.json()
+        table_html, _ = create_table(data_json)
+        display(HTML(table_html))
+    else:
+        data_json = data.json()
+        df = pd.DataFrame(data_json)
+        return df
