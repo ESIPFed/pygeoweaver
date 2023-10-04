5ec3e7486eda9b09dd8a6147059778bf75b5b003
Gokul Prathin <gokulprathin8@gmail.com>
2023-10-03 21:08:42 -0400

    final testing changes and import optimizations

--- pygeoweaver/sc_detail.py
+++ pygeoweaver/sc_detail.py
@@ -65,11 +65,11 @@ def detail_host(host_id):
     url = f"{GEOWEAVER_DEFAULT_ENDPOINT_URL}/web/detail"
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     form_data = {'type': 'host', 'id': host_id}
-    d = requests.post(url=url, data=form_data, headers=headers)
+    d = requests.post(url=url, data=form_data, headers=headers).json()
     try:
         from IPython import get_ipython
         if 'IPKernelApp' in get_ipython().config:
-            table_html = create_table([d.json()])
+            table_html = create_table([d])
             table_output = widgets.Output()
             with table_output:
                 display(HTML(table_html))
