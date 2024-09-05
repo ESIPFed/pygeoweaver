89346f0b7ad0975d22c8290d3b7bb836bb4cf455
Ziheng Sun <zsun@gmu.edu>
2024-09-05 01:52:10 -0400

    use sys env port

--- pygeoweaver/constants.py
+++ pygeoweaver/constants.py
@@ -1,4 +1,7 @@
-GEOWEAVER_DEFAULT_ENDPOINT_URL = "http://localhost:8070/Geoweaver"
+import os
+
+GEOWEAVER_PORT=os.getenv("GEOWEAVER_PORT", "8070")
+GEOWEAVER_DEFAULT_ENDPOINT_URL = f"http://localhost:{GEOWEAVER_PORT}/Geoweaver"
 COMMON_API_HEADER = {"Content-Type": "application/json"}
 GEOWEAVER_URL = (
     "https://github.com/ESIPFed/Geoweaver/releases/download/latest/geoweaver.jar"
